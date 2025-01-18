9454fe5caaefd52e575b89e04c08169a40bdcc56
wd021 <dannywitters@gmail.com>
2025-01-18 18:16:16 +0000

    use permit2

--- src/GotVision.sol
+++ src/GotVision.sol
@@ -3,12 +3,51 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+
+interface IPermit2 {
+    struct TokenPermissions {
+        address token;
+        uint256 amount;
+    }
+
+    struct PermitTransferFrom {
+        TokenPermissions permitted;
+        uint256 nonce;
+        uint256 deadline;
+    }
+
+    struct SignatureTransferDetails {
+        address to;
+        uint256 requestedAmount;
+    }
+
+    function permitTransferFrom(
+        PermitTransferFrom calldata permit,
+        SignatureTransferDetails calldata transferDetails,
+        address owner,
+        bytes calldata signature
+    ) external;
+}
+
+/*
+ #####                  #     #                         
+#     #  ####  #####    #     # #  ####  #  ####  #    #
+#       #    #   #      #     # # #      # #    # ##   #
+#  #### #    #   #      #     # #  ####  # #    # # #  #
+#     # #    #   #       #   #  #      # # #    # #  # #
+#     # #    #   #        # #   # #    # # #    # #   ##
+ #####   ####    #         #    #  ####  #  ####  #    #
+*/
 
 contract GotVision is Ownable, ReentrancyGuard, Pausable {
     IERC20 public wldToken;
-    
+
+    // Add Permit2 constant and interface
+    address public constant PERMIT2_ADDRESS = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+    IPermit2 public immutable permit2;
+
     // Events with timestamps
     event Deposited(address indexed user, uint256 amount, uint256 timestamp);
     event Withdrawn(address indexed user, uint256 amount, uint256 timestamp);
@@ -17,18 +56,33 @@ contract GotVision is Ownable, ReentrancyGuard, Pausable {
     constructor(address _wldToken) Ownable(msg.sender) {
         require(_wldToken != address(0), "Invalid token address");
         wldToken = IERC20(_wldToken);
+        permit2 = IPermit2(PERMIT2_ADDRESS);
     }
-    
+
     /**
-     * @notice Allows users to deposit WLD tokens
+     * @notice Allows users to deposit WLD tokens using Permit2
      * @param amount The amount of WLD tokens to deposit
+     * @param permit The Permit2 permission structure
+     * @param signature The signature for the Permit2 transfer
      */
-    function deposit(uint256 amount) external whenNotPaused nonReentrant {
+    function deposit(
+        uint256 amount,
+        IPermit2.PermitTransferFrom calldata permit,
+        bytes calldata signature
+    ) external whenNotPaused nonReentrant {
         require(amount > 0, "Amount must be greater than 0");
+        require(permit.permitted.amount >= amount, "Insufficient permit amount");
         
-        // Transfer tokens from user to contract
-        require(wldToken.transferFrom(msg.sender, address(this), amount), 
-                "Transfer failed");
+        // Execute the Permit2 transfer
+        permit2.permitTransferFrom(
+            permit,
+            IPermit2.SignatureTransferDetails({
+                to: address(this),
+                requestedAmount: amount
+            }),
+            msg.sender,
+            signature
+        );
         
         // Emit deposit event with current timestamp
         emit Deposited(msg.sender, amount, block.timestamp);
